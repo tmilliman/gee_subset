40059217d41ec5c4415d94be6ca17ac69a9037a1
Tom Milliman <thomas.milliman@unh.edu>
2018-08-23 14:01:36 -0400

    pep8 cleanup and fix/incorporate verbose argument

--- gee_subset/gee_subset.py
+++ gee_subset/gee_subset.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
 # Google Earth Engine (GEE) Subset package
-# 
+#
 # Easy subsetting of remote sensing
 # time series for processing external
 # to GEE.
-# 
+#
 # This in parts replaces the ORNL DAAC
 # MODIS subsets, but extends it to higher
 # resolution date such as Landsat and
@@ -14,86 +14,91 @@
 # product / band syntax.
 
 # load required libraries
-import os, argparse
+import os
+import argparse
 from datetime import datetime
 import pandas as pd
 import ee
+
 
 # parse arguments in a beautiful way
 # includes automatic help generation
 def getArgs():
 
-   # setup parser
-   parser = argparse.ArgumentParser(
-    description = '''Google Earth Engine subsets script: 
-                    Allows for the extraction of remote sensing product
-                    time series for most of the data available on
-                    Google Earth Engine. Locations need to be specied as
-                    either a comma delimited file or an explicit latitude
-                    and longitude using command line arguments.''',
-    epilog = '''post bug reports to the github repository''')
-   parser.add_argument('-p',
-                       '--product',
-                       help = 'remote sensing product available in GEE',
-                       required = True)
-                       
-   parser.add_argument('-b',
-                       '--bands',
-                       help = 'band name(s) for the requested product',
-                       nargs = "+",
-                       required = True)
-                       
-   parser.add_argument('-s',
-                       '--start',
-                       help = 'start date of the time series (yyyy-mm-dd)',
-                       default = "2013-01-01")
-                       
-   parser.add_argument('-e',
-                       '--end',
-                       help = 'end date of the time series (yyyy-mm-dd)',
-                       default = "2014-12-31")
-                       
-   parser.add_argument('-pd',
-                       '--pad',
-                       help = '''grow sampling location 
-                       in km east west north south''',
-                       default = 0,
-                       type = float)
+    # setup parser
+    parser = argparse.ArgumentParser(
+       description='''Google Earth Engine subsets script:
+       Allows for the extraction of remote sensing product
+       time series for most of the data available on
+       Google Earth Engine. Locations need to be specied as
+       either a comma delimited file or an explicit latitude
+       and longitude using command line arguments.''',
+       epilog="post bug reports to the github repository")
 
-   parser.add_argument('-sc',
-                       '--scale',
-                       help = '''scale in meter, match the native resolution of
-                       the data of interest otherwise mismatches in scale will result in
-                       high pixel counts and a system error''',
-                       default = "30")
+    parser.add_argument('-p',
+                        '--product',
+                        help='remote sensing product available in GEE',
+                        required=True)
 
-   parser.add_argument('-l',
-                       '--location',
-                       nargs = 2,
-                       help = '''geographic location as latitude longitude
-                       provided as -loc latitude longitude''',
-                       default = 0,
-                       type = float)
+    parser.add_argument('-b',
+                        '--bands',
+                        help='band name(s) for the requested product',
+                        nargs="+",
+                        required=True)
 
-   parser.add_argument('-f',
-                       '--file',
-                       help = '''path to file with geographic locations
+    parser.add_argument('-s',
+                        '--start',
+                        help='start date of the time series (yyyy-mm-dd)',
+                        default="2013-01-01")
+
+    parser.add_argument('-e',
+                        '--end',
+                        help='end date of the time series (yyyy-mm-dd)',
+                        default="2014-12-31")
+
+    parser.add_argument('-pd',
+                        '--pad',
+                        help='''grow sampling location in km
+                        east west north south''',
+                        default=0,
+                        type=float)
+
+    parser.add_argument('-sc',
+                        '--scale',
+                        help='''scale in meter, match the native resolution of
+                        the data of interest otherwise mismatches in scale
+                        will result in high pixel counts and a system error''',
+                        default="30")
+
+    parser.add_argument('-l',
+                        '--location',
+                        nargs=2,
+                        help='''geographic location as latitude longitude
+                        provided as -loc latitude longitude''',
+                        default=None,
+                        type=float)
+
+    parser.add_argument('-f',
+                        '--file',
+                        help='''path to file with geographic locations
                         as provided in a csv file''',
-                       default = 0)
-                       
-   parser.add_argument('-d',
-                       '--directory',
-                       help = '''directory / path where to write output when not
-                       provided this defaults to output to the console''',
-                       default = 0)  
-                                        
-   parser.add_argument('-v',
-                       '--verbose',
-                       help = '''verbose debugging''',
-                       default = False)  
-   # put arguments in dictionary with
-   # keys being the argument names given above
-   return parser.parse_args()
+                        default=0)
+
+    parser.add_argument('-d',
+                        '--directory',
+                        help='''directory / path where to write output when not
+                        provided this defaults to output to the console''',
+                        default=0)
+
+    parser.add_argument('-v',
+                        '--verbose',
+                        help='''verbose debugging''',
+                        default=False,
+                        action="store_true")
+
+    # put arguments in dictionary with
+    # keys being the argument names given above
+    return parser.parse_args()
 
 # GEE subset subroutine 
 def gee_subset(product = None,
@@ -159,17 +164,37 @@ if __name__ == "__main__":
 
    # parse arguments
    args = getArgs()
+   verbose = args.verbose
+   product = args.product
+   bands = args.bands
+   location_file = args.file
+   location = args.location
+   start_date = args.start
+   end_date = args.end
    
+   
+   if verbose:
+      print("verbose: {}".format(verbose))
+      print("location: {}".format(location))
+      print("product: {}".format(product))
+      print("bands: {}".format(bands))
+      print("location file: {}".format(location_file))
+      print("location: {}".format(location))
+      print("start date: {}".format(start_date))
+      print("end date: {}".format(end_date))
+      
+      
    # read in locations if they exist,
    # overrides the location argument
-   if args.file:
-      if os.path.isfile(args.file):
+   if location_file is not None:
+      if os.path.isfile(location_file):
         if args.location:
             print("not a valid location file, check path") 
         else:
-          locations = pd.read_csv(args.file)
+          locations = pd.read_csv(location_file)
+          print(locations)
    else:
-      locations = pd.DataFrame(['site'] + args.location).transpose()  	 
+      locations = pd.DataFrame(['site'] + location).transpose()  	 
    
    # initialize GEE session
    # requires a valid authentication token
